f0288a29d307d885fbaa4e34abe2d5438a142123
mebesius <odin_mebesius@yahoo.ca>
2018-10-21 13:34:34 -0700

    trying to get the front end working oml

--- app/src/main/java/com/example/android/carbonfootprint/MainActivity.java
+++ app/src/main/java/com/example/android/carbonfootprint/MainActivity.java
@@ -1,10 +1,18 @@
 package com.example.android.carbonfootprint;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.sql.Timestamp;
 import java.util.Date;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,8 +20,60 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+        final WordListAdapter adapter = new WordListAdapter(this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
 
 
+}
+
+public class WordListAdapter extends RecyclerView.Adapter<WordListAdapter.WordViewHolder> {
+
+    class WordViewHolder extends RecyclerView.ViewHolder {
+        private final TextView wordItemView;
+
+        private WordViewHolder(View itemView) {
+            super(itemView);
+            wordItemView = itemView.findViewById(R.id.textView);
+        }
+    }
+
+    private final LayoutInflater mInflater;
+    private List<Entry> mEntry; // Cached copy of entries
+
+    WordListAdapter(Context context) { mInflater = LayoutInflater.from(context); }
+
+    @Override
+    public WordViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = mInflater.inflate(R.layout.recyclerview_item, parent, false);
+        return new WordViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(WordViewHolder holder, int position) {
+        if (mEntry != null) {
+            Entry current = mEntry.get(position);
+            holder.wordItemView.setText(current.getEntry());
+        } else {
+            // Covers the case of data not being ready yet.
+            holder.wordItemView.setText("No Entry");
+        }
+    }
+
+    void setEntry(List<Entry> entries) {
+        mEntry = entries;
+        notifyDataSetChanged();
+    }
+
+    // getItemCount() is called many times, and when it is first called,
+    // mEntry has not been updated (means initially, it's null, and we can't return null).
+    @Override
+    public int getItemCount() {
+        if (mEntry != null)
+            return mEntry.size();
+        else return 0;
+    }
 }
